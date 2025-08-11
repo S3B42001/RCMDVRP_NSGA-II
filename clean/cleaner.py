11a221428a0cbb3ce30fa0c723b0789ac0b4d11a
S3B42001 <s.andres.b2001@gmail.com>
2025-08-11 17:00:39 -0400

    AMPL Run Files

--- clean/cleaner.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-with open(sys.argv[1]) as result:
-        uniqlines = set(result.readlines())
-        with open(sys.argv[2], 'w') as rmdup:
-            rmdup.writelines(set(uniqlines))
